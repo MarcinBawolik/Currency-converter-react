96af757844b05aeb8f8ecd4e6b03277f82cb5655
Marcin Bawolik <marcinbawolik2000@gmail.com>
2023-02-09 16:19:40 +0100

    deleted unnecessary things

--- src/AcctualDate/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyledAcctualDate = styled.div`
-    font-family: 'Space Mono', monospace;
-    display: grid;
-    justify-content: end;
-    padding: 16px;
-    color: rgb(147, 147, 147);
-`;
