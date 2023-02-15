e35d993572772315156ae1cf39984458ee147ada
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-02-15 16:51:00 +0100

    calculateResault transferred to Form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import  Form  from "./Form";
-import { currencies } from "./currencies";
-import { useState } from "react";
+
 
 function App() {
  
-  const [result, setResult] = useState();
 
-  const calculateResault = ({currency, amountToBeConverted}) => {
-    const rate = currencies
-    .find(({ shortName }) => shortName === currency)
-    .rate;
-
-    setResult({
-      sourceAmount: +amountToBeConverted,
-      targetAmount: amountToBeConverted / rate,
-      currency,
-    });
-  }
   return (
     <div>
-      <Form 
-      calculateResault={calculateResault}
-      result={result}
-      />
+      <Form />
     </div>
   )
 }
