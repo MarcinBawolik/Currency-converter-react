95dc53c5167ea17d5fbdb68b4492cc9aebb24003
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-02-15 16:51:31 +0100

    added new color crimson

--- src/theme.js
+++ src/theme.js
@@ -4,7 +4,8 @@ teal: "#008080",
 white: "#FFFFFF",
 black: "#000000",
 gray: "#808080",
-silverchalice: "#9E9E9E"
+silverchalice: "#9E9E9E",
+crimson: "#db143c"
     },
     breakpoint: {
         mobileMax: 767,
