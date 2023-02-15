795be904dbed51c1447a75c7e6b18becfc438f16
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-02-15 16:52:50 +0100

    added api.exchangerate.host  to form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,21 +1,44 @@
 import { useState, useRef } from "react";
-import { currencies } from "../currencies";
 import { Result } from "../Result";
 import { ActualDate } from "../ActualDate";
-import { StyledForm, FormButton, FormParagraph, FormHeader, FormSelect, FormSpan } from "./styled";
+import {
+    StyledForm,
+    FormButton,
+    FormParagraph,
+    FormHeader,
+    FormSelect,
+    FormSpan,
+    Loading,
+    Failure
+} from "./styled";
+import { useRatesData } from "../useRatesData";
+import { useCurrentDate } from "../useCurrentDate";
 
-
-const Form = ({ calculateResault, result }) => {
-    const [currency, setCurrency] = useState(currencies[0].shortName);
-    const [amountToBeConverted, setAmountToBeConverted] = useState("");
+const Form = () => {
     const inputRef = useRef(null)
-    
+
+    const [result, setResult] = useState();
+    const ratesData = useRatesData();
+
+    const calculateResault = (currency, amountToBeConverted) => {
+        const rate = ratesData.rates[currency];
+
+        setResult({
+            sourceAmount: +amountToBeConverted,
+            targetAmount: amountToBeConverted * rate,
+            currency,
+        });
+    };
+
+    const [currency, setCurrency] = useState("EUR");
+    const [amountToBeConverted, setAmountToBeConverted] = useState("");
+
     const onFormSubmit = (event) => {
         event.preventDefault();
-        calculateResault({ amountToBeConverted, currency });
+        calculateResault(amountToBeConverted, currency);
     }
-    
-    const  focusInput = () => {
+
+    const focusInput = () => {
         inputRef.current.focus();
     }
 
@@ -25,50 +48,72 @@ const Form = ({ calculateResault, result }) => {
             <FormHeader>
                 Przelicznik walut
             </FormHeader>
-            <p>
-                <label>
-                    <FormSpan>
-                        Kwota w zł*
-                    </FormSpan>
-                    <FormSelect as="input"
-                        ref={inputRef}
-                        value={amountToBeConverted}
-                        placeholder="Wpisz kwotę w zł"
-                        type="number"
-                        required
-                        step="0.01"
-                        onChange={({ target }) => setAmountToBeConverted(target.value)}
-                    />
-                </label>
-            </p>
-            <p>
-                <label>
-                    <FormSpan>
-                        Waluta:
-                    </FormSpan>
-                    <FormSelect
-                        value={currency}
-                        className="form__field"
-                        onChange={({ target }) => setCurrency(target.value)}
-                    >
-                        {currencies.map((currency => (
-                            <option
-                                key={currency.shortName}
-                                value={currency.shortName}
-                            >
-                                {currency.name}
-                            </option>
-                        )))}
+            {ratesData.state === "loading"
+                ? (
+                    <Loading>
+                        Sekunda... <br /> Ładuje kursy z Europejskiego Banku Centralnego.
+                    </Loading>
+                )
+                : (
+                    ratesData.state === "error" ? (
+                        <Failure>
+                            Coś poszło nie tak. Sprawdź swoje połączenie z internetem.
+                        </Failure>
+                    )
+                        : (
+                            <>
+                                <p>
+                                    <label>
+                                        <FormSpan>
+                                            Kwota w zł*
+                                        </FormSpan>
+                                        <FormSelect as="input"
+                                            ref={inputRef}
+                                            value={amountToBeConverted}
+                                            placeholder="Wpisz kwotę w zł"
+                                            type="number"
+                                            required
+                                            step="0.01"
+                                            onChange={({ target }) => setAmountToBeConverted(target.value)}
+                                        />
+                                    </label>
+                                </p>
+                                <p>
+                                    <label>
+                                        <FormSpan>
+                                            Waluta:
+                                        </FormSpan>
+                                        <FormSelect
+                                            value={currency}
+                                            className="form__field"
+                                            onChange={({ target }) => setCurrency(target.value)}
+                                        >
+                                            {Object.keys(ratesData.rates).map(((currency) => (
+                                                <option
+                                                    key={currency}
+                                                    value={currency}
+                                                >
+                                                    {currency}
+                                                </option>
+                                            )))}
 
-                    </FormSelect>
-                </label>
-            </p>
-            <p>
-                <FormButton onClick={focusInput}>Przelicz!</FormButton>
-            </p>
+                                        </FormSelect>
+                                    </label>
+                                </p>
+                                <p>
+                                    <FormButton onClick={focusInput}>Przelicz!</FormButton>
+                                </p>
+                            </>
+                        )
+                )
+            }
+
             <FormParagraph>
-                Kursy pochodzą ze strony nbp.pl z Tabeli nr 009/C/NBP/2023 z dnia 2023-01-12
-
+                Kursy walut pobierane są z Europejskiego Banku Centralnego <br />
+                Aktualne na dzień {useCurrentDate().toLocaleDateString(
+                undefined,
+                { day: "numeric", month: "long", year: "numeric" }
+            )}
             </FormParagraph>
             <Result result={result} />
         </StyledForm>
